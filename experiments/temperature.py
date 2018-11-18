5a41ff40dccbc7e6b56eb7cb6f345af629302a0c
neryah <neryahadad@gmail.com>
2018-11-18 20:57:11 +0200

    20: temperature

--- experiments/temperature.py
+++ experiments/temperature.py
@@ -3,8 +3,23 @@ from matplotlib import pyplot as plt
 
 X = np.array([400, 450, 900, 390, 550])
 
-# TODO: Write the code as explained in the instructions
-raise NotImplemented()  # TODO: remove!
+T=np.linspace(0.01, 5, 100)
+min = np.amin(X)
+
+#squared=np.array(map(lambda x:)
+P = np.zeros((len(T),len(X)))
+
+for i in range(len(X)):
+    x_array = np.zeros(len(T))
+    for temp_ind, temp in enumerate(T):
+        exp = -1/temp
+        squared = np.array(list(map(lambda x: (x/min)**exp, X)))
+        squared_sum = np.sum(squared)
+        top = (X[i]/min)**exp
+        x_array[temp_ind] = (top / squared_sum)
+
+    P[:, i] = x_array
+
 
 print(P)
 
